e1261039ca872ae97b62a953e7472331f600f0c8
pk <pradeephellofriend@gmail.com>
2025-05-29 12:54:31 +0530

    seo

---
+++ ecosystem.config.cjs
@@ -0,0 +1,13 @@
+//require("dotenv").config();
+module.exports = {
+  apps: [
+    {
+      name: 'Kamsoft-Technologies',
+      cwd: '/home/projects/frontend/kamsoft_web/',
+      script: 'npm',
+      args: 'start',
+      env:{},
+      
+    },
+  ],
+};
