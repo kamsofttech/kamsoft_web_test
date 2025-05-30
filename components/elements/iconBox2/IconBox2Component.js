d44642e0e354c3ae9b7155917ab609e7636c8895
pk <pradeephellofriend@gmail.com>
2025-05-30 14:19:10 +0530

    update

--- components/elements/iconBox2/IconBox2Component.js
+++ components/elements/iconBox2/IconBox2Component.js
@@ -6,7 +6,7 @@ const IconBox2Component = ({video}) => {
   return (
     <div className={styles.iconBox2}>
         <ReactPlayer url={video} className='video-box' width={"100%"} height={"100%"}/>
-        <span >Working with hi-soft</span>
+        <span >Working with Kamsoft Technologies</span>
     </div>
   )
 }
